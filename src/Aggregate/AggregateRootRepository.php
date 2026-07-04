8a9f6c0a3b4a20955e941b0af3b1f3095230fbc7
pizzaminded <pizzaminded@users.noreply.github.com>
2026-07-04 13:52:37 +0200

    add `AggregrateRootRepository`

---
+++ src/Aggregate/AggregateRootRepository.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jadob\Scribe\Aggregate;
+
+use DateTime;
+use Jadob\Scribe\Aggregate\Exception\NoEventsRecordedException;
+use Jadob\Scribe\Aggregate\Exception\UnsupportedAggregateTypeException;
+use Jadob\Scribe\Event\MessageRepositoryInterface;
+use Jadob\Scribe\Message\Message;
+use Jadob\Scribe\Message\MessageHeader;
+
+use function count;
+use function get_class;
+
+/**
+ * @template T of AggregateRootInterface
+ */
+readonly class AggregateRootRepository
+{
+    /**
+     * @param class-string<T> $aggregateRootClass
+     */
+    public function __construct(
+        private string $aggregateRootClass,
+        private MessageRepositoryInterface $messageRepository,
+    ) {
+    }
+
+    /**
+     * @throws UnsupportedAggregateTypeException
+     * @throws NoEventsRecordedException
+     */
+    public function store(AggregateRootInterface $aggregateRoot): void
+    {
+        $aggregateFqcn = get_class($aggregateRoot);
+        $this->assertAggregateType($aggregateFqcn);
+        $aggregateId = $aggregateRoot->getAggregateRootId();
+        $events = $aggregateRoot->popEvents();
+
+        if (count($events) === 0) {
+            throw new NoEventsRecordedException(sprintf('No events found for aggregate %s', $aggregateFqcn));
+        }
+
+        $messages = [];
+        foreach ($events as $revision => $event) {
+            $messages[] = Message::create($event)
+                ->withHeader(MessageHeader::AGGREGATE_ID, $aggregateId->toString())
+                ->withHeader(MessageHeader::AGGREGATE_REVISION, $revision)
+                ->withHeader(MessageHeader::AGGREGATE_TYPE, $aggregateFqcn)
+                ->withHeader(MessageHeader::RECORDED_AT, new DateTime()->getTimestamp());
+        }
+
+        $this
+            ->messageRepository
+            ->store(...$messages);
+    }
+
+    /**
+     * @throws UnsupportedAggregateTypeException
+     */
+    private function assertAggregateType(
+        string $aggregateRootClass,
+    ): void {
+        if ($this->aggregateRootClass !== $aggregateRootClass) {
+            throw new UnsupportedAggregateTypeException(sprintf('Invalid aggregate type (received "%s", repository configured for "%s")', $aggregateRootClass, $this->aggregateRootClass));
+        }
+    }
+}
---
+++ tests/Aggregate/AggregateRootRepositoryTest.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jadob\Scribe\Aggregate;
+
+use Jadob\Scribe\Aggregate\Exception\NoEventsRecordedException;
+use Jadob\Scribe\Aggregate\Exception\UnsupportedAggregateTypeException;
+use Jadob\Scribe\Aggregate\Id\UuidAggregateId;
+use Jadob\Scribe\Aggregate\Id\UuidBinaryAggregateId;
+use Jadob\Scribe\Event\MessageRepositoryInterface;
+use Jadob\Scribe\Fixtures\Aggregate\Order;
+use Jadob\Scribe\Fixtures\Aggregate\User;
+use Jadob\Scribe\Fixtures\Event\UserCreatedEvent;
+use Jadob\Scribe\Message\Message;
+use Jadob\Scribe\Message\MessageHeader;
+use PHPUnit\Framework\TestCase;
+
+class AggregateRootRepositoryTest extends TestCase
+{
+    public function testRepositoryWillPreventSavingAggregateNotMeantToBeUsedForHisType(): void
+    {
+        $repository = new AggregateRootRepository(
+            Order::class,
+            $this->createStub(MessageRepositoryInterface::class)
+        );
+
+        $this->expectException(UnsupportedAggregateTypeException::class);
+        $this->expectExceptionMessage(
+            'Invalid aggregate type (received "Jadob\Scribe\Fixtures\Aggregate\User", repository configured for "Jadob\Scribe\Fixtures\Aggregate\Order")'
+        );
+
+        $repository->store(User::create(
+            UuidBinaryAggregateId::new7(),
+            'jdoe',
+            'jdoe@example.com'
+        ));
+    }
+
+    public function testRepositoryWillPreventSavingAggregateWithoutEvents(): void
+    {
+        $repository = new AggregateRootRepository(
+            User::class,
+            $this->createStub(MessageRepositoryInterface::class)
+        );
+
+        $this->expectException(UnsupportedAggregateTypeException::class);
+        $this->expectExceptionMessage(
+            'Invalid aggregate type (received "Jadob\Scribe\Fixtures\Aggregate\User", repository configured for "Jadob\Scribe\Fixtures\Aggregate\Order")'
+        );
+
+        $user = User::create(
+            UuidBinaryAggregateId::new7(),
+            'jdoe',
+            'jdoe@example.com'
+        );
+
+        $user->popEvents();
+
+        $this->expectException(NoEventsRecordedException::class);
+        $this->expectExceptionMessage('No events found for aggregate Jadob\Scribe\Fixtures\Aggregate\User');
+
+        $repository->store($user);
+    }
+
+    public function testRepositoryWrapEventsIntoMessageWithMetadata(): void
+    {
+        $repository = new AggregateRootRepository(
+            User::class,
+            $messageRepositoryMock = $this->createMock(MessageRepositoryInterface::class)
+        );
+
+        $aggregateId = UuidAggregateId::new7();
+        $user = User::create(
+            $aggregateId,
+            'jdoe',
+            'jdoe@example.com'
+        );
+
+        $messageRepositoryMock
+            ->expects($this->once())
+            ->method('store')
+            ->willReturnCallback(
+                function (Message ...$messages) use ($aggregateId): void {
+                    self::assertCount(1, $messages);
+
+                    $message = $messages[0];
+                    self::assertInstanceOf(UserCreatedEvent::class, $message->event);
+                    self::assertSame($aggregateId->toString(), $message->headers[MessageHeader::AGGREGATE_ID]);
+                    self::assertSame(1, $message->headers[MessageHeader::AGGREGATE_REVISION]);
+                    self::assertSame(User::class, $message->headers[MessageHeader::AGGREGATE_TYPE]);
+                }
+            );
+
+        $repository->store($user);
+    }
+}
