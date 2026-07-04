b8dc859416b54a04f71804d3694f0fb4afa3609f
pizzaminded <pizzaminded@users.noreply.github.com>
2026-07-04 14:20:33 +0200

    add `EventPayload` attribute

---
+++ src/Event/EventPayload.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jadob\Scribe\Event;
+
+use Attribute;
+
+/**
+ * This attribute is optional.
+ * If you leave these values default, it will be serialized as-is.
+ * If you enable encryption, an encryption key will be generated, persisted to separate table and encrypted value will
+ * be stored here. However, it is up to message serializer if this will happen.
+ */
+#[Attribute(Attribute::TARGET_PROPERTY)]
+final readonly class EventPayload
+{
+    public function __construct(
+        private(set) ?string $key = null,
+        private bool $encrypted = false
+    ) {
+    }
+}
