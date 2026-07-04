19e329d9a3d4409f7381cdc72f015f20d42f0b05
pizzaminded <pizzaminded@users.noreply.github.com>
2026-07-04 03:17:55 +0200

    add `Message`

---
+++ src/Message/Message.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jadob\Scribe\Message;
+
+final readonly class Message
+{
+    /**
+     * @param array<string, string|int> $headers
+     */
+    private function __construct(
+        private(set) object $event,
+        private(set) array  $headers = [],
+    )
+    {
+    }
+
+    public static function create(object $event): self
+    {
+        return new self($event);
+    }
+
+    public function withHeader(
+        string     $key,
+        string|int $value
+    ): self
+    {
+        $headers = $this->headers;
+        $headers[$key] = $value;
+
+        return new self(
+            $this->event,
+            $headers
+        );
+    }
+}
