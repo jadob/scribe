53d2aa310067323cd57a22ca7f5ddfdd5c3a157d
pizzaminded <pizzaminded@users.noreply.github.com>
2026-07-04 14:21:37 +0200

    add exceptions

---
+++ src/Aggregate/Exception/NoEventsRecordedException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jadob\Scribe\Aggregate\Exception;
+
+use Jadob\Scribe\Exception\ScribeException;
+
+final class NoEventsRecordedException extends ScribeException
+{
+}
---
+++ src/Exception/ScribeException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jadob\Scribe\Exception;
+
+use Exception;
+
+class ScribeException extends Exception
+{
+}
