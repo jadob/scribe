0f39897773813b817c9f6c2af100ca3c1315b3fa
pizzaminded <pizzaminded@users.noreply.github.com>
2026-07-10 23:18:27 +0200

    add serializer implementation

---
+++ src/Message/Serializer/MessageSerializer.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jadob\Scribe\Message\Serializer;
+
+use Jadob\Scribe\Message\Message;
+use Jadob\Scribe\Message\MessageHeader;
+
+final readonly class MessageSerializer implements MessageSerializerInterface
+{
+    public function __construct(
+        private MessageNormalizerInterface $normalizer,
+        private EventNameInflectorInterface $inflector,
+    ) {
+    }
+
+    public function serialize(Message $message): string
+    {
+        $event = $message->event;
+        $eventType = $this
+            ->inflector
+            ->fromFqcn(\get_class($event));
+
+        $message = $message
+            ->withHeader(MessageHeader::EVENT_TYPE, $eventType);
+
+        $normalizedMessage = $this
+            ->normalizer
+            ->normalize(
+                $message
+            );
+
+        return \json_encode(
+            $normalizedMessage,
+            \JSON_THROW_ON_ERROR);
+    }
+}
